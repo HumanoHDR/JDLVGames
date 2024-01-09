9de81496153a40b60bcb81d0091daa1aacc9e162
JoseDavidCamara <113546851+JoseDavidCamara@users.noreply.github.com>
2024-01-09 18:29:30 +0100

    comprobaciones aciertos y fallos

--- JoseDavid/js/pokemon.js
+++ JoseDavid/js/pokemon.js
@@ -1,8 +1,8 @@
- // Función para obtener un Pokémon al azar desde la PokeAPI
- var pokemoninicial = []
+// Función para obtener un Pokémon al azar desde la PokeAPI
+var pokemoninicial = []
 
- obtenerPokemonRandom();
- function obtenerPokemonRandom() {
+obtenerPokemonRandom();
+function obtenerPokemonRandom() {
   // Limpiar el contenido previo
   // Obtener un número aleatorio para el Pokémon
   var pokemonId = Math.floor(Math.random() * 898 + 1);
@@ -49,21 +49,24 @@
           pokemoninicial[0] = data.id;
 
           // Tipos del Pokémon en español
-          tipos= data.types.map(type => tiposEnEspanol[type.type.name]).join(", ");
+          tipos = data.types.map(type => tiposEnEspanol[type.type.name]).join(", ");
           tipos = tipos.split(",");
 
           pokemoninicial[1] = tipos[0]
 
-          if(tipos[1] == null) pokemoninicial[2] = tipos[0];
+          if (tipos[1] == null) pokemoninicial[2] = tipos[0];
           else pokemoninicial[2] = tipos[1];
 
-          
+
           // Generación del Pokémon
           pokemoninicial[3] = obtenerGeneracion(dataEspecie.generation.name);
 
           // Nombre del Pokémon
           pokemoninicial[4] = data.name;
-          console.log(pokemoninicial[4]); //
+          pokemoninicial[5] = data.weight;
+          pokemoninicial[6] = data.height / 10;
+
+          console.log(pokemoninicial); //
 
         })
         .catch(errorEspecie => console.error('Error al obtener detalles de la especie del Pokémon', errorEspecie));
@@ -126,66 +129,110 @@ function obtenerEvoluciones(evolvesFromSpecies) {
 
 comprobar = document.getElementById("Comprobar");
 
-comprobar.addEventListener("click", function(){
-    nombrePokemonUsuario = document.getElementById("nombrePokemon").value;
-    nombrePokemonUsuario = nombrePokemonUsuario.toLowerCase();
-    console.log(nombrePokemonUsuario);
-    if (nombrePokemonUsuario != "")
-    {
-      errores = document.getElementById("error");
-      fetch(`https://pokeapi.co/api/v2/pokemon/${nombrePokemonUsuario}/`)
-                .then(response => response.json())
-                .then(pokemon => {
-                pokemonid = pokemon.id
-                  fetch(`https://pokeapi.co/api/v2/pokemon-species/${pokemonid}/`)
-                      .then(response => response.json())
-                      .then(dataEspecie => {
-                        divcreation = document.createElement("div")
-                        divcreation.classList.add("resultado");
-                        
-                        generacionPokemon = document.createElement("div");
-                        generacionPokemon.classList.add("r3")
-                        generacionPokemon.innerText = obtenerGeneracion(dataEspecie.generation.name);
-                        divcreation.appendChild(generacionPokemon);
-                        
-                        /*nombrePoke = document.createElement("div");
-                        nombrePoke.classList.add("r4");
-                        nombrePoke.innerHTML = pokemon.name;
-                        divcreation.appendChild(nombrePoke);*/
+comprobar.addEventListener("click", function () {
+  nombrePokemonUsuario = document.getElementById("nombrePokemon").value;
+  nombrePokemonUsuario = nombrePokemonUsuario.toLowerCase();
+  console.log(nombrePokemonUsuario);
+  if (nombrePokemonUsuario != "") {
+    errores = document.getElementById("error");
+    fetch(`https://pokeapi.co/api/v2/pokemon/${nombrePokemonUsuario}/`)
+      .then(response => response.json())
+      .then(pokemon => {
+        pokemonid = pokemon.id
+        fetch(`https://pokeapi.co/api/v2/pokemon-species/${pokemonid}/`)
+          .then(response => response.json())
+          .then(dataEspecie => {
+            divcreation = document.createElement("div")
+            divcreation.classList.add("resultado");
 
-                        tipos= pokemon.types.map(type => tiposEnEspanol[type.type.name]).join(", ");
-                        tipos = tipos.split(",");
+            generacionPokemon = document.createElement("div");
+            generacionPokemon.classList.add("r3")
+            let gen = obtenerGeneracion(dataEspecie.generation.name);
+            generacionPokemon.innerText = gen;
+            divcreation.appendChild(generacionPokemon);
 
-                        tipo1Pokemon = document.createElement("div");
-                        tipo1Pokemon.classList.add("r1");
-                        
-                        tipo1Pokemon.innerText = tipos[0];
-                        divcreation.appendChild(tipo1Pokemon);
+            /*nombrePoke = document.createElement("div");
+            nombrePoke.classList.add("r4");
+            nombrePoke.innerHTML = pokemon.name;
+            divcreation.appendChild(nombrePoke);*/
 
-                        tipo2Pokemon = document.createElement("div");
-                        tipo2Pokemon.classList.add("r2");
-                        if(tipos[1] == null) tipo2Pokemon.innerText = tipos[0];
-                        else tipo2Pokemon.innerText = tipos[1];
+            tipos = pokemon.types.map(type => tiposEnEspanol[type.type.name]).join(", ");
+            tipos = tipos.split(",");
 
-                        pesopokemon = document.createElement("div");
-                        pesopokemon.classList.add("r5");
-                        pesopokemon.innerText = pokemon.weight + "kg";
-                        divcreation.appendChild(pesopokemon);
+            tipo1Pokemon = document.createElement("div");
+            tipo1Pokemon.classList.add("r1");
 
-                        alturapokemon = document.createElement("div");
-                        alturapokemon.classList.add("r4");
-                          altura = pokemon.height / 10
-                        alturapokemon.innerText = altura + " m";
-                        divcreation.appendChild(alturapokemon);
 
-                        divcreation.appendChild(tipo2Pokemon);
-                        divtitulos.insertAdjacentElement('afterend',divcreation);
-                        console.log("exito")
-                      })
-                    })
-    }
+            tipo1Pokemon.innerText = tipos[0];
+            divcreation.appendChild(tipo1Pokemon);
+
+            tipo2Pokemon = document.createElement("div");
+            tipo2Pokemon.classList.add("r2");
+            if (tipos[1] == null) tipos[1] = tipos[0];
+            tipo2Pokemon.innerText = tipos[1];
+
+            pesopokemon = document.createElement("div");
+            pesopokemon.classList.add("r5");
+            let peso = pokemon.weight;
+            pesopokemon.innerText = peso+ "kg";
+            divcreation.appendChild(pesopokemon);
+
+            alturapokemon = document.createElement("div");
+            alturapokemon.classList.add("r4");
+            altura = pokemon.height / 10
+            alturapokemon.innerText = altura + " m";
+            divcreation.appendChild(alturapokemon);
+
+            divcreation.appendChild(tipo2Pokemon);
+            console.log(pokemoninicial)
+            if (tipos[0] === pokemoninicial[1]) {
+              tipo1Pokemon.classList.add("acierto");
+            } else {
+              tipo1Pokemon.classList.add("fallo");
+            }
+            if (tipos[1] === pokemoninicial[2]) {
+              tipo2Pokemon.classList.add("acierto");
+            } else {
+              tipo2Pokemon.classList.add("fallo");
+            }
+            if (gen === pokemoninicial[3]) {
+              generacionPokemon.classList.add("acierto");
+            } else {
+              generacionPokemon.classList.add("fallo");
+            }
+            if (peso > pokemoninicial[5]) {
+              pesopokemon.innerText = "↓ "+peso+ "kg";
+              divcreation.appendChild(pesopokemon);
+            } 
+            else if (peso < pokemoninicial[5]) {
+              pesopokemon.innerText = "↑ "+peso+ "kg";
+              divcreation.appendChild(pesopokemon);
+            }
+            else if (peso == pokemoninicial[5]) {
+              pesopokemon.innerText = peso+ "kg";
+              pesopokemon.classList.add("acierto");
+              divcreation.appendChild(pesopokemon);
+            }
+            if (altura == pokemoninicial[6]) {
+              alturapokemon.classList.add("acierto");
+              alturapokemon.innerText = altura + " m";
+              divcreation.appendChild(alturapokemon);
+            } else if(altura > pokemoninicial[6]){
+              alturapokemon.innerText = "↓ "+altura + " m";
+              divcreation.appendChild(alturapokemon);
+            } else if(altura < pokemoninicial[6]){
+              alturapokemon.innerText = "↑ "+altura + " m";
+              divcreation.appendChild(alturapokemon);
+            }
+
+
+            divtitulos.insertAdjacentElement('afterend', divcreation);
+            console.log("exito")
+          })
+      })
+  }
 });
 
 function mostrarPokemon(pokemon) {
-  
+
 };
